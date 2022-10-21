df4e5bc53557e29421dd60f9995248b6962ffdba
ValentainFomkin <valentain1625@mail.ru>
2022-10-21 13:56:03 +0300

    change ways Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Header} from "antd/es/layout/layout";
+import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navbar/Navbar";
 import {Dialogs} from "./components/Dialogs/Dialogs";
 import {Profile} from "./components/Profile/Profile";
@@ -10,21 +10,21 @@ import {Music} from "./components/Music/Music";
 import {Settings} from "./components/Settings/Settings";
 
 export const App = () => {
-  return (
-      <BrowserRouter>
-          <div className='app-wrapper'>
-              <Header/>
-              <Navbar/>
-              <div className='app-wrapper-content'>
-                  <Route path='/dialogs' component={Dialogs}/>
-                  <Route path='/profile' component={Profile}/>
-                  <Route path='/news' component={News}/>
-                  <Route path='/music' component={Music}/>
-                  <Route path='/settings' component={Settings}/>
-              </div>
-          </div>
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Route path='/dialogs' component={Dialogs}/>
+                    <Route path='/profile' component={Profile}/>
+                    <Route path='/news' component={News}/>
+                    <Route path='/music' component={Music}/>
+                    <Route path='/settings' component={Settings}/>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 
