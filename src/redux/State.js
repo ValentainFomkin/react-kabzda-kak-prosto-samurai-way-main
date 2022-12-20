5b0fc5a7b9e7a8f1d5cae40d6aca554cfd37bfe2
ValentainFomkin <valentain1625@mail.ru>
2022-12-20 18:59:08 +0300

    refactored cmpn

--- src/redux/State.js
+++ src/redux/State.js
@@ -8,12 +8,36 @@ export let state = {
     },
     messagesPages: {
         dialogs: [
-            {id: 1, name: 'Dimych'},
-            {id: 2, name: 'Andrey'},
-            {id: 3, name: 'Sveta'},
-            {id: 4, name: 'Sasha'},
-            {id: 5, name: 'Viktor'},
-            {id: 6, name: 'Valera'},
+            {
+                id: 1,
+                name: 'Dimych',
+                avatar: 'https://lifehacker.ru/special/fujifilm/dist/static/img/5.2410a2d.jpg'
+            },
+            {
+                id: 2,
+                name: 'Andrey',
+                avatar: 'http://www.rosphoto.com/images/u/articles/1510/7_5.jpg'
+            },
+            {
+                id: 3,
+                name: 'Sveta',
+                avatar: 'https://bigpicture.ru/wp-content/uploads/2015/11/nophotoshop29-800x532.jpg'
+            },
+            {
+                id: 4,
+                name: 'Sasha',
+                avatar: 'http://www.rosphoto.com/images/u/articles/1510/11_2.jpg'
+            },
+            {
+                id: 5,
+                name: 'Viktor',
+                avatar: 'https://vjoy.cc/wp-content/uploads/2020/10/30d52c581db742197509da30df.jpg'
+            },
+            {
+                id: 6,
+                name: 'Valera',
+                avatar: 'https://weblinks.ru/wp-content/uploads/2021/04/636fvtrvidk.jpg'
+            },
         ],
         messages: [
             {id: 1, message: 'Hi'},
@@ -21,5 +45,25 @@ export let state = {
             {id: 3, message: 'Yo'},
         ],
     },
+    siteBarPages: {
+        friends: [
+            {
+                id: 1,
+                name: 'Dimych',
+                avatar: 'https://lifehacker.ru/special/fujifilm/dist/static/img/5.2410a2d.jpg'
+            },
+            {
+                id: 2,
+                name: 'Andrey',
+                avatar: 'http://www.rosphoto.com/images/u/articles/1510/7_5.jpg'
+            },
+            {
+                id: 3,
+                name: 'Sveta',
+                avatar: 'https://bigpicture.ru/wp-content/uploads/2015/11/nophotoshop29-800x532.jpg'
+            },
+        ]
+
+    }
 
 }
