fdf9f3f7d91f73c9b8e9ef04627be664a7c7479f
ValentainFomkin <valentain1625@mail.ru>
2022-12-20 18:58:41 +0300

    refactored cmpn

---
+++ src/components/Friends/FriendsItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+
+export const FriendsItem = (props) => {
+    let path = '/friends/' + props.id
+    return (
+        <div>
+            <NavLink to={path}>{props.name}</NavLink>
+        </div>
+    );
+};
+
