b65e1f5cf210f63bbfa664bcf2b49c5e1353c7c4
ValentainFomkin <valentain1625@mail.ru>
2022-12-22 17:29:10 +0300

    create new component

---
+++ src/components/ImagesForPages/ImagesForPages.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import s from './ImagesForPages.module.css'
+import {NavLink} from "react-router-dom";
+
+
+export const ImagesForPages = () => {
+    return (
+        <nav className={s.nav}>
+            <div className={s.item}>
+                <NavLink to='/profile'>Profile</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/dialogs'>Messages</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/news'>News</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/music'>Music</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/settings'>Settings</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/friends'>Friends</NavLink>
+                <div className={s.photoUnderFriends}>
+                    <img src="https://blog.hootsuite.com/wp-content/uploads/2021/07/free-stock-photos-03-scaled.jpeg"
+                         alt=""/>
+                    <img
+                        src="https://blog.hootsuite.com/wp-content/uploads/2021/07/free-stock-photos-03-scaled.jpeg"
+                        alt=""/>
+                    <img src="https://blog.hootsuite.com/wp-content/uploads/2021/07/free-stock-photos-03-scaled.jpeg"
+                         alt=""/>
+                </div>
+
+
+            </div>
+        </nav>
+    )
+}
+
