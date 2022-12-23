62652d686c58fea63a34c79c641a01e1ccf17f63
ValentainFomkin <valentain1625@mail.ru>
2022-12-23 15:58:25 +0300

    props addPost

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import {News} from "./components/News/News";
 import {Music} from "./components/Music/Music";
 import {Settings} from "./components/Settings/Settings";
 import {Friends} from "./components/Friends/Friends";
+import {addPost} from "./redux/State";
 
 export type AppPropsType = {
     // posts: PostsProps[]
@@ -26,6 +27,7 @@ export type AppPropsType = {
             friends: FriendsProps[]
         }
     }
+    addPost: (postMessage: string) => void
 }
 
 export type FriendsProps = {
@@ -53,20 +55,18 @@ export type PostsProps = {
 
 export const App = (props: AppPropsType) => {
     return (
-
         <div className='app-wrapper'>
             <Header/>
             <Navbar/>
             <div className='app-wrapper-content'>
                 <Routes>
                     <Route path='/dialogs' element={<Dialogs state={props.state.messagesPages}/>}/>
-                    <Route path='/profile' element={<Profile state={props.state.profilePages}/>}/>
+                    <Route path='/profile' element={<Profile state={props.state.profilePages} addPost={addPost}/>}/>
                     <Route path='/news' element={<News/>}/>
                     <Route path='/music' element={<Music/>}/>
                     <Route path='/settings' element={<Settings/>}/>
                     <Route path='/friends' element={<Friends state={props.state.siteBarPages}/>}/>
                 </Routes>
-
             </div>
         </div>
 
