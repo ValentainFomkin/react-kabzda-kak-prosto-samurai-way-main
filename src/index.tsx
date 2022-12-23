48c42ca6316843c1d62055f771e45b4a5e785ecb
ValentainFomkin <valentain1625@mail.ru>
2022-12-23 16:31:25 +0300

    remove logic and call func rerender

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import {App} from './App';
-import {addPost, state} from "./redux/State";
-import {BrowserRouter} from "react-router-dom";
+import {renderEntireTree} from "./render";
+import {state} from "./redux/State";
 
-
-ReactDOM.render(
-    <BrowserRouter>
-        <App state={state}
-             addPost={addPost}
-        />
-    </BrowserRouter>,
-    document.getElementById('root')
-);
+renderEntireTree(state)
