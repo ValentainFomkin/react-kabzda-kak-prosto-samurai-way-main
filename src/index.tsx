ea479172e2ab0547a627802e50092b1116b7c1e3
ValentainFomkin <valentain1625@mail.ru>
2022-12-24 20:36:18 +0300

    again (:D) rename component for  best working

---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './index.css';
+import {store} from "./redux/State";
+
+import ReactDOM from "react-dom";
+import {BrowserRouter} from "react-router-dom";
+import {App} from "./App";
+
+
+const renderEntireTree = (state: any) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App _state={state}
+                 addPost={store.addPost.bind(store)}
+                 updateNewPostText={store.updateNewPostText.bind(store)}
+                 addMessage={store.addMessage.bind(store)}
+                 updateNewMessageText={store.updateNewMessageText.bind(store)}
+            />
+        </BrowserRouter>,
+        document.getElementById('root')
+    );
+}
+
+
+renderEntireTree(store.getState())
+
+store.subscribe(renderEntireTree)
