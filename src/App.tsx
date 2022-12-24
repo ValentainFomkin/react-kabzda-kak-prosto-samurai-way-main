4a32d06c5bb579183ec117bd20581db37d822e4a
ValentainFomkin <valentain1625@mail.ru>
2022-12-24 20:36:07 +0300

    again (:D) rename component for  best working

--- src/App.tsx
+++ src/App.tsx
@@ -10,50 +10,50 @@ import {Music} from "./components/Music/Music";
 import {Settings} from "./components/Settings/Settings";
 import {Friends} from "./components/Friends/Friends";
 
-// export type AppPropsType = {
-//     // posts: PostsProps[]
-//     // dialogs: DialogsProps[]
-//     // messages: MessagesProps[]
-//     state: {
-//         profilePages: {
-//             posts: PostsProps[]
-//             newPostText: string
-//         }
-//         messagesPages: {
-//             dialogs: DialogsProps[]
-//             messages: MessagesProps[]
-//             newMessageText: string
-//         }
-//         siteBarPages: {
-//             friends: FriendsProps[]
-//         }
-//     }
-//     addPost: () => void
-//     updateNewPostText: (newText: string) => void
-//     addMessage: () => void
-//     updateNewMessageText: (newText: string) => void
-// }
-// export type FriendsProps = {
-//     id: number
-//     name: string
-//     avatar: string
-// }
-// export type MessagesProps = {
-//     id: number
-//     message: string
-// }
-// export type DialogsProps = {
-//     id: number
-//     name: string
-//     avatar: string
-// }
-// export type PostsProps = {
-//     id: number,
-//     message: string,
-//     likesCount: number
-// }
+export type AppPropsType = {
+    // posts: PostsProps[]
+    // dialogs: DialogsProps[]
+    // messages: MessagesProps[]
+    _state: {
+        profilePages: {
+            posts: PostsProps[]
+            newPostText: string
+        }
+        messagesPages: {
+            dialogs: DialogsProps[]
+            messages: MessagesProps[]
+            newMessageText: string
+        }
+        siteBarPages: {
+            friends: FriendsProps[]
+        }
+    }
+    addPost: () => void
+    updateNewPostText: (newText: string) => void
+    addMessage: () => void
+    updateNewMessageText: (newText: string) => void
+}
+export type FriendsProps = {
+    id: number
+    name: string
+    avatar: string
+}
+export type MessagesProps = {
+    id: number
+    message: string
+}
+export type DialogsProps = {
+    id: number
+    name: string
+    avatar: string
+}
+export type PostsProps = {
+    id: number,
+    message: string,
+    likesCount: number
+}
 
-export const App = (props) => {
+export const App = (props: AppPropsType) => {
     return (
         <div className='app-wrapper'>
             <Header/>
@@ -61,19 +61,19 @@ export const App = (props) => {
             <div className='app-wrapper-content'>
                 <Routes>
                     <Route path='/dialogs' element={<Dialogs
-                        state={props.state.messagesPages}
+                        state={props._state.messagesPages}
                         addMessage={props.addMessage}
                         updateNewMessageText={props.updateNewMessageText}
                     />}/>
                     <Route path='/profile'
                            element={<Profile
-                               profilePages={props.state.profilePages}
+                               profilePages={props._state.profilePages}
                                addPost={props.addPost}
                                updateNewPostText={props.updateNewPostText}/>}/>
                     <Route path='/news' element={<News/>}/>
                     <Route path='/music' element={<Music/>}/>
                     <Route path='/settings' element={<Settings/>}/>
-                    <Route path='/friends' element={<Friends state={props.state.siteBarPages}/>}/>
+                    <Route path='/friends' element={<Friends state={props._state.siteBarPages}/>}/>
                 </Routes>
             </div>
         </div>
