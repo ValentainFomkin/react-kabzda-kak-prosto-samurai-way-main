7a092216c1c66b245a9b024a7da35509171e73d7
ValentainFomkin <valentain1625@mail.ru>
2022-12-25 15:43:36 +0300

    change work the reducer

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,4 +1,30 @@
-const profileReducer = (state, action) => {
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
-    return state
+export const profileReducer = (state, action) => {
+
+    switch (action.type) {
+        case ADD_POST: {
+            let newPost = {
+                id: 3,
+                message: state.newPostText,
+                likesCount: 123
+            }
+            if (state.newPostText) {
+                state.posts.unshift(newPost)
+                state.newPostText = ''
+            }
+            return state
+        }
+        case UPDATE_NEW_POST_TEXT: {
+            return state.newPostText = action.newTextForPost
+        }
+        default:
+            return state
+    }
+
+
 }
+
+export const addPostActionCreator = () => ({type: ADD_POST})
+export const updateNewPostTextActionCreator = (value) => ({type: UPDATE_NEW_POST_TEXT, newTextForPost: value})
