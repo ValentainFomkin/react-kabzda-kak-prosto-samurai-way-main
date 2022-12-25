e71165daf4de44f0af18f6d2e3aef3da979b2dcc
ValentainFomkin <valentain1625@mail.ru>
2022-12-25 15:43:44 +0300

    change work the reducer

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,4 +1,29 @@
-const dialogsReducer = (state, action) => {
+const ADD_MESSAGE = 'ADD-MESSAGE';
+const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT';
 
-    return state
+export const dialogsReducer = (state, action) => {
+
+    switch (action.type) {
+        case ADD_MESSAGE: {
+            let newMessage = {
+                id: 4,
+                message: state.newMessageText,
+                likesCount: 23
+            }
+            if (state.newMessageText) {
+                state.messages.push(newMessage)
+                state.newMessageText = ''
+            }
+            return state
+        }
+        case UPDATE_NEW_MESSAGE_TEXT: {
+            return state.newMessageText = action.newTextForMessage
+        }
+        default:
+            return state
+    }
+
 }
+
+export const addNewMessageActionCreator = () => ({type: ADD_MESSAGE})
+export const onChangeMessageActionCreator = (value) => ({type: UPDATE_NEW_MESSAGE_TEXT, newTextForMessage: value})
