b42ea9ba19ad18160c7a4cc2ffe08b9ebb5d2013
ValentainFomkin <valentain1625@mail.ru>
2022-12-25 19:53:52 +0300

    added initial state

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,7 +1,15 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
-export const profileReducer = (state, action) => {
+let initialState = {
+    posts: [
+        {id: 1, message: 'Hi, how are you', likesCount: 50},
+        {id: 2, message: 'It\'s my first post', likesCount: 21},
+    ],
+    newPostText: '',
+}
+
+export const profileReducer = (state = initialState, action) => {
 
     switch (action.type) {
         case ADD_POST: {
