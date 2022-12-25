0a4db39fde43f25757f24d1a10205161e71654cf
ValentainFomkin <valentain1625@mail.ru>
2022-12-25 19:54:07 +0300

    added initial state

--- src/redux/siteBar-reducer.js
+++ src/redux/siteBar-reducer.js
@@ -1,4 +1,25 @@
-export const siteBarReducer = (state, action) => {
+let initialState = {
+    friends: [
+        {
+            id: 1,
+            name: 'Dimych',
+            avatar: 'https://lifehacker.ru/special/fujifilm/dist/static/img/5.2410a2d.jpg'
+        },
+        {
+            id: 2,
+            name: 'Andrey',
+            avatar: 'http://www.rosphoto.com/images/u/articles/1510/7_5.jpg'
+        },
+        {
+            id: 3,
+            name: 'Sveta',
+            avatar: 'https://bigpicture.ru/wp-content/uploads/2015/11/nophotoshop29-800x532.jpg'
+        },
+    ]
+
+}
+
+export const siteBarReducer = (state = initialState, action) => {
 
     return state
 }
