980cac2950d76d121bf2211b198958b6bd201681
ValentainFomkin <valentain1625@mail.ru>
2022-12-25 15:44:13 +0300

    change import AC

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import s from './Dialogs.module.css'
 import {DialogItem} from "./DialogItem/DialogItem";
 import {Message} from "./Message/Message";
-import {addNewMessageActionCreator, onChangeMessageActionCreator} from "../../redux/State";
+import {addNewMessageActionCreator, onChangeMessageActionCreator} from "../../redux/dialogs-reducer";
 
 
 export const Dialogs = (props) => {
